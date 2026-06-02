b01caa246ed1c402e085ac8c8d7b88b336494007
mit raval <hareshraval8140@gmail.com>
2026-06-02 11:55:30 +0530

    implement all calculator functions

--- calculator.py
+++ calculator.py
@@ -2,21 +2,38 @@ class calculator:
     
     def menu(self):
         print('''
+THIS IS A SIMPLE CALCULATOR!
               PRESS 1:SUM
               PRESS 2:MULTIPLY
-              PRESS 3:MINUS
+              PRESS 3:SUBTRACTION
               PRESS 4:DIVISON
               PRESS 5:PERCENTAGE
               PRESS 6:EXIT
 
         ''')
-        choice=input("ENTER YOUR CHOICE:-")
         num1=int(input("ENTER FIRST NUMBER:-"))
         num2=int(input("ENTER SECOND NUMBER:-"))
+        
+        choice=input("ENTER YOUR CHOICE:-")
         
         if choice =='1':
              num3=num1+num2
              print("YOUR SUM=",num3)
+        elif choice =='2':
+            num3=num1*num2
+            print("YOUR MULTIPLY=",num3)
+        elif choice =='3':
+            num3=num1-num2
+            print("YOUR MINUS=",num3)
+        elif choice =='4':
+            num3=num1/num2
+            print("YOUR DIVISION=",num3)
+        elif choice =='5':
+            num3=(num1/num2)*100
+            print("YOUR PERCENTAGE=",num3)
+        elif choice=='6':
+            exit()
+        
 
 C1=calculator()
 C1.menu()
