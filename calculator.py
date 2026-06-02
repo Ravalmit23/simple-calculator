9328180c32e0a58baa04d0d8e21c6aa1b783dd2b
mit raval <hareshraval8140@gmail.com>
2026-06-02 08:52:20 +0530

    add sum using if-else

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
 class calculator:
     
     def menu(self):
-        
         print('''
               PRESS 1:SUM
               PRESS 2:MULTIPLY
@@ -11,11 +10,13 @@ class calculator:
               PRESS 6:EXIT
 
         ''')
-        choice=int(input("ENTER YOUR CHOICE:-"))
+        choice=input("ENTER YOUR CHOICE:-")
         num1=int(input("ENTER FIRST NUMBER:-"))
         num2=int(input("ENTER SECOND NUMBER:-"))
         
-
+        if choice =='1':
+             num3=num1+num2
+             print("YOUR SUM=",num3)
 
 C1=calculator()
 C1.menu()
