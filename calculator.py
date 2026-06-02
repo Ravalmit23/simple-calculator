be6d9fb6a5d7000a127376e64ebe2ea9132d6e14
mit raval <hareshraval8140@gmail.com>
2026-06-02 08:40:34 +0530

    create menu of calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,21 @@
 class calculator:
-    num1=int(input("ENTER FIRST NUMBER:-"))
-    num2=int(input("ENTER SECOND NUMBER:-"))
+    
+    def menu(self):
+        
+        print('''
+              PRESS 1:SUM
+              PRESS 2:MULTIPLY
+              PRESS 3:MINUS
+              PRESS 4:DIVISON
+              PRESS 5:PERCENTAGE
+              PRESS 6:EXIT
+
+        ''')
+        choice=int(input("ENTER YOUR CHOICE:-"))
+        num1=int(input("ENTER FIRST NUMBER:-"))
+        num2=int(input("ENTER SECOND NUMBER:-"))
+        
+
+
+C1=calculator()
+C1.menu()
